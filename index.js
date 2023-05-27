4784e6d965e1fa4491a0239cd01965aeed182b89
IconicLuck <uhasib959@gmail.com>
2023-05-27 20:43:24 +0600

    created doctor services fetch link

--- index.js
+++ index.js
@@ -26,6 +26,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const doctorServicesCollection = client.db('docHouseDB').collection('doctorServices')
+
+    app.get('/doctorServices', async(req, res) => {
+        const result = await doctorServicesCollection.find().toArray()
+        res.send(result)
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
